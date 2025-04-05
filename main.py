9ed4d51bcea5da0ba79c21059fe09c4cb6648284
ErnestWanjohi <ernestwanjohi35@gmail.com>
2025-04-05 09:38:00 +0300

    done and dusted

---
+++ main.py
@@ -0,0 +1,17 @@
+a = int(input("Enter First Number: "))
+b = str(input("Enter Operator: " ))
+c = int(input("Enter second member: "))
+if (b == "+"):
+    print(a+c)
+
+elif (b == "-"):
+    print(a-c)
+
+elif (b == "*"):
+    print(a*c)
+
+elif (b=="/"):
+    print(a/c)
+
+
+
